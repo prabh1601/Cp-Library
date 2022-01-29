fd782add1f1045380a04fc7d7735bf58bac7961b
Prabh <prabh1601@gmail.com>
2022-01-29 10:47:05 +0530

    generalized scripts

--- StressTest/Without_Checker/stress.sh
+++ StressTest/Without_Checker/stress.sh
@@ -1,22 +1,29 @@
 set -e
-g++ -std=c++20 test__Generator.cpp -o /mnt/3E86036E860325CD/CP/TestCases/gen || { echo -e "\n\e[1;31mCompilation Error : \e[0mGenerator\n"; exit 1; }
-g++ -std=c++20 test.cpp -o /mnt/3E86036E860325CD/CP/TestCases/test || { echo -e "\n\e[1;31mCompilation Error : \e[0mTestFile\n"; exit 1; }
-g++ -std=c++20 test__Good.cpp -o /mnt/3E86036E860325CD/CP/TestCases/good || { echo -e "\n\e[1;31mCompilation Error : \e[0mGoodFile\n"; exit 1; }
+
+# relative location to store the (build and data) files
+relative_directory='/mnt/3E86036E860325CD/CP/TestCases'
+
+#compiling the cpp files
+g++ -std=c++20 test__Generator.cpp -o $relative_directory/gen || { echo -e "\n\e[1;31mCompilation Error : \e[0mGenerator\n"; exit 1; }
+g++ -std=c++20 test.cpp -o $relative_directory/test || { echo -e "\n\e[1;31mCompilation Error : \e[0mTestFile\n"; exit 1; }
+g++ -std=c++20 test__Good.cpp -o $relative_directory/good || { echo -e "\n\e[1;31mCompilation Error : \e[0mGoodFile\n"; exit 1; }
 
 echo -e -n "\n\e[1;36mCompilation Successful\e[0m\n"
 
+#running stress
 for((i = 1; ; ++i)); do
-    /mnt/3E86036E860325CD/CP/TestCases/gen $i > /mnt/3E86036E860325CD/CP/TestCases/input_file
-    /mnt/3E86036E860325CD/CP/TestCases/test < /mnt/3E86036E860325CD/CP/TestCases/input_file > /mnt/3E86036E860325CD/CP/TestCases/myAnswer
-    /mnt/3E86036E860325CD/CP/TestCases/good < /mnt/3E86036E860325CD/CP/TestCases/input_file > /mnt/3E86036E860325CD/CP/TestCases/correctAnswer
-    diff -Z /mnt/3E86036E860325CD/CP/TestCases/myAnswer /mnt/3E86036E860325CD/CP/TestCases/correctAnswer > /dev/null || break
     echo -e -n "\033[1;32m\rRunning Test : \033[0m" $i
+    $relative_directory/gen $i > $relative_directory/input_file
+    $relative_directory/test < $relative_directory/input_file > $relative_directory/myAnswer
+    $relative_directory/good < $relative_directory/input_file > $relative_directory/correctAnswer
+    diff -Z $relative_directory/myAnswer $relative_directory/correctAnswer > /dev/null || break
 done
 
+#printing out the results
 printf "\n\n\e[1;33mCounter Test\e[0m\n"
-cat /mnt/3E86036E860325CD/CP/TestCases/input_file
+cat $relative_directory/input_file
 printf "\n\e[1;33mCorrect Answer\e[0m\n"
-cat /mnt/3E86036E860325CD/CP/TestCases/correctAnswer 
+cat $relative_directory/correctAnswer 
 printf "\n\e[1;33mYour Answer\e[0m\n"
-cat /mnt/3E86036E860325CD/CP/TestCases/myAnswer 
+cat $relative_directory/myAnswer 
 printf "\n\n"
--- StressTest/Without_Checker/test__Good.cpp
+++ StressTest/Without_Checker/test__Good.cpp
@@ -4,8 +4,6 @@ using namespace std;
 
 #define int long long
 #define ld long double
-#define ff first
-#define ss second
 #define pb push_back
 #define mag(v) (int)v.size()
 #define all(v) begin(v), end(v)
@@ -15,45 +13,14 @@ const int N = 2e5 + 10;  // verify
 template <typename T> bool ckmin(T& a, T b) { return b < a && (a = b, true); }
 template <typename T> bool ckmax(T& a, T b) { return b > a && (a = b, true); }
 
-void testCase() {
-    int n, m;
-    cin >> n >> m;
-    vector<set<pair<int, int>>> a(2);
-    for (int r = 0; r < 2; r++) {
-        for (int i = 0; i < m; i++) {
-            int u, v;
-            cin >> u >> v;
-            u--, v--;
-            if (u > v) swap(u, v);
-            a[r].insert({u, v});
-        }
-    }
-
-    vector<int> p(n);
-    iota(all(p), 0);
-
-    auto chk = [&]() {
-        bool pos = true;
-        for (auto [u, v] : a[0]) {
-            int uu = p[u], vv = p[v];
-            if (uu > vv) swap(uu, vv);
-            pos &= (a[1].count({uu, vv}));
-        }
-
-        return pos;
-    };
-
-    while (true) {
-        if (chk()) { return void(cout << "Yes"); }
-        if (!next_permutation(all(p))) break;
-    }
-
-    cout << "No";
-}
+void testCase() { cout << "NO"; }
 
 int32_t main() {
     cin.tie(0)->sync_with_stdio(0);
     int t_c = 1;
     // cin >> t_c;
-    while (t_c--) testCase();
+    for (int testNo = 1; testNo <= t_c; testNo++) {
+        // cout << "Case #" << t << ": ";
+        testCase();
+    }
 }
