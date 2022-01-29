0dbb4d016408a670df66c4198de2abc2f32397ee
Prabh <prabh1601@gmail.com>
2022-01-29 10:44:46 +0530

    generalized stress scripts

--- StressTest/With_Checker/runChecker.sh
+++ StressTest/With_Checker/runChecker.sh
@@ -1,16 +1,22 @@
 set -e
-g++ -std=c++17 test__Generator.cpp -o /mnt/3E86036E860325CD/CP/TestCases/gen || { echo -e "\n\e[1;31mCompilation Error : \e[0mGenerator\n"; exit 1; }
-g++ -std=c++17 test.cpp -o /mnt/3E86036E860325CD/CP/TestCases/test || { echo -e "\n\e[1;31mCompilation Error : \e[0mTestFile\n"; exit 1; }
-g++ -std=c++17 test__Good.cpp -o /mnt/3E86036E860325CD/CP/TestCases/good || { echo -e "\n\e[1;31mCompilation Error :\e[0mGoodFile\n"; exit 1; }
-g++ -std=c++17 test__checker.cpp -o /mnt/3E86036E860325CD/CP/TestCases/checker || { echo -e "\n\e[1;31mCompilation Error : \e[0mChecker\n"; exit 1; }
+
+#relative location to store the files
+relative_directory='/mnt/3E86036E860325CD/CP/TestCases'
+
+#compiling the cpp files
+g++ -std=c++17 test__Generator.cpp -o $relative_directory/gen || { echo -e "\n\e[1;31mCompilation Error : \e[0mGenerator\n"; exit 1; }
+g++ -std=c++17 test.cpp -o $relative_directory/test || { echo -e "\n\e[1;31mCompilation Error : \e[0mTestFile\n"; exit 1; }
+g++ -std=c++17 test__Good.cpp -o $relative_directory/good || { echo -e "\n\e[1;31mCompilation Error :\e[0mGoodFile\n"; exit 1; }
+g++ -std=c++17 test__checker.cpp -o $relative_directory/checker || { echo -e "\n\e[1;31mCompilation Error : \e[0mChecker\n"; exit 1; }
 
 echo -e -n "\n\e[1;36mCompilation Successful\e[0m\n"
 
+#running stress
 for((i = 1; ; ++i)); do
-	/mnt/3E86036E860325CD/CP/TestCases/gen $i > /mnt/3E86036E860325CD/CP/TestCases/input_file
-    /mnt/3E86036E860325CD/CP/TestCases/test < /mnt/3E86036E860325CD/CP/TestCases/input_file > /mnt/3E86036E860325CD/CP/TestCases/myAnswer
-    /mnt/3E86036E860325CD/CP/TestCases/good < /mnt/3E86036E860325CD/CP/TestCases/input_file > /mnt/3E86036E860325CD/CP/TestCases/correctAnswer
-    /mnt/3E86036E860325CD/CP/TestCases/checker > /mnt/3E86036E860325CD/CP/TestCases/checker_log
+	$relative_directory/gen $i > $relative_directory/input_file
+    $relative_directory/test < $relative_directory/input_file > $relative_directory/myAnswer
+    $relative_directory/good < $relative_directory/input_file > $relative_directory/correctAnswer
+    $relative_directory/checker > $relative_directory/checker_log
     echo -e -n "\033[1;32m\rRunning Test : \033[0m" $i
 done
     
--- StressTest/With_Checker/stress.sh
+++ StressTest/With_Checker/stress.sh
@@ -1,12 +1,15 @@
 set -e
 $bash ./runChecker.sh
 
+relative_directory='/mnt/3E86036E860325CD/CP/TestCases'
+
+#printing out the results
 printf "\n\e[1;33mCounter Test\e[0m\n"
-cat /mnt/3E86036E860325CD/CP/TestCases/input_file
+cat $relative_directory/input_file
 printf "\n\e[1;33mCorrect Answer\e[0m\n"
-cat /mnt/3E86036E860325CD/CP/TestCases/correctAnswer 
+cat $relative_directory/correctAnswer 
 printf "\n\e[1;33mYour Answer\e[0m\n"
-cat /mnt/3E86036E860325CD/CP/TestCases/myAnswer 
+cat $relative_directory/myAnswer 
 printf "\n\e[1;33mChecker's Verdict\e[0m\n"
-cat /mnt/3E86036E860325CD/CP/TestCases/checker_log 
+cat $relative_directory/checker_log 
 printf "\n"
