f71b4227a8171d74c5d7c77f3f60afb01618185e
prabh <prabh1601@gmail.com>
2022-04-05 22:46:01 +0530

    fixed lazy segtree templating issue

--- StressTest/Without_Checker/test_Good.cpp
+++ StressTest/Without_Checker/test_Good.cpp
@@ -1,8 +1,72 @@
-#include <bits/stdc++.h>
+// Come on mate, keep going, we ain't that weak
+#include "bits/stdc++.h"
 using namespace std;
 
-int main() {
-    int x = rand() % 2;
-    cout << (x ? "NO\n" : "YES\n");
-    cout << 1;
+#ifdef PRABH
+#include "debug.h"
+#else
+#define dbg(...)
+#pragma GCC optimize("Ofast,unroll-loops")  // careful, this might break floating point arithmatic
+#pragma GCC target("avx,avx2,sse4.2,bmi,bmi2,popcnt,lzcnt")
+#endif
+
+template <class Fun> class y_combinator_result {
+    Fun fun_;
+
+   public:
+    template <class T> explicit y_combinator_result(T&& fun) : fun_(std::forward<T>(fun)) {}
+
+    template <class... Args> decltype(auto) operator()(Args&&... args) {
+        return fun_(std::ref(*this), std::forward<Args>(args)...);
+    }
+};
+
+template <class Fun> decltype(auto) y_combinator(Fun&& fun) {
+    return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun));
+}
+
+void yes(bool caps = true) { cout << (caps ? "YES" : "Yes") << '\n'; }
+void no(bool caps = true) { cout << (caps ? "NO" : "No") << '\n'; }
+
+template <typename T> bool ckmin(T& a, T b) { return b < a && (a = b, true); }
+template <typename T> bool ckmax(T& a, T b) { return b > a && (a = b, true); }
+
+#define int int64_t
+#define pb push_back
+#define all(v) begin(v), end(v)
+using ld = long double;
+const int inf = 4e18;
+const int N = 2e5 + 10;  // verify before using
+
+void testCase() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int& i : a) {
+        cin >> i;
+    }
+
+    vector<int> ans(n + 1);
+    for (int d = 1; d <= n; d++) {
+        int& s = ans[d];
+        set<int> vis;
+        for (int i = 0; !vis.count(i); i = (i + d) % n) {
+            s += a[i];
+            vis.insert(i);
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        cout << ans[i] << '\n';
+    }
+}
+
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int t_c = 1;
+    // cin >> t_c;
+    for (int testNo = 1; testNo <= t_c; testNo++) {
+        // cout << "Case #" << testNo << ": ";
+        testCase();
+    }
 }
